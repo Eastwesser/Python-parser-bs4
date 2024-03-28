e7f0a7dbe9eae4b93f6a648304b097af07ad6c79
Dennis Mattews <alttert@mail.ru>
2024-03-28 04:06:20 +0300

    simple parse lists

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import xlsxwriter
 from bs4 import BeautifulSoup
 
 main_url = 'https://www.edimdoma.ru'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
 
 data = [['Наименование', 'Список', 'Cсылка']]
+
 
 # Function to fetch HTML content from a URL
 def get_html(url):
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.text
+
 
 # Fetch HTML content from the website
 try:
@@ -31,7 +34,7 @@ try:
             data.append([subcategory_name, category_name, subcategory_url])
 
     # Write data to Excel file
-    with xlsxwriter.Workbook('recipes.xlsx') as workbook:
+    with xlsxwriter.Workbook('dishes_list.xlsx') as workbook:
         worksheet = workbook.add_worksheet()
 
         for row_num, row_data in enumerate(data):
